bc6ce0a21d25c4f9a5aefa8ceff57c6b2598236a
likhithlumen <likhith.br@lumen.com>
2025-03-03 02:00:48 +0530

    Create program4.java

---
+++ program4.java
@@ -0,0 +1,18 @@
+
+//Write a Java program to get the character at the given index within the string.
+
+import java.util.*;
+
+public class program4
+{
+	public static void main(String[] args)
+	{
+		String s = "lumen";
+		
+		Scanner sc = new Scanner(System.in);
+		int b= sc.nextInt();
+		System.out.println(s.charAt(b));
+	}
+}
+	
+	
