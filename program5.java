e6588acc98513686a41664ea79a3a1fbc45ebc3b
likhithlumen <likhith.br@lumen.com>
2025-03-03 02:05:21 +0530

    Create program5.java

---
+++ program5.java
@@ -0,0 +1,14 @@
+
+//Write a Java program to concatenate a given string to the end of another string
+
+import java.util.*;
+
+public class program5
+{
+	public static void main(String[] args)
+	{
+		String s = "lumen";
+		System.out.println(s.concat(" technologies"));
+	}
+}
+	
