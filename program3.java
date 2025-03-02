2ff4575fde8f13a503530a7eef2e9b5bfe550070
likhithlumen <likhith.br@lumen.com>
2025-03-03 01:55:26 +0530

    Create program3.java

---
+++ program3.java
@@ -0,0 +1,22 @@
+//Write a Java program to copy all mappings from the specified map to another map.
+
+import java.util.*;
+public class program1
+{
+	public static void main(String[] agrs)
+	{
+		HashMap<Integer,String> empid = new HashMap<>();
+		empid.put(1,"ramu");
+		empid.put(2, "naveen");
+		empid.put(3, "veer");
+		
+		HashMap<Integer,String> empid2 = new HashMap<>();
+		
+		empid2.putAll(empid);
+		
+		System.out.println(empid2);
+		
+		
+	}
+}
+	
